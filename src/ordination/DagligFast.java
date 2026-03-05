6a1681a205e8ba138461e2ad32c36870d4c4aaef
AkselTimmermann <aksel.timmermann@gmail.com>
2026-03-05 10:27:51 +0100

    DagligFast færdig

--- src/ordination/DagligFast.java
+++ src/ordination/DagligFast.java
@@ -4,20 +4,32 @@ package ordination;
 import gui.TypeOrdination;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class DagligFast extends Ordination{
     // TODO
-    private Dosis[] doser;
+    private Dosis[] doser = new Dosis[4];
 
+    public DagligFast(LocalDate startDen, LocalDate slutDen, double morgenAntal, double middagAntal, double aftenAntal, double natAntal) {
+        super(startDen, slutDen);
+        doser[0] = new Dosis(LocalTime.of(8,0), morgenAntal);
+        doser[1] = new Dosis(LocalTime.of(12,0), middagAntal);
+        doser[2] = new Dosis(LocalTime.of(18,0), aftenAntal);
+        doser[3] = new Dosis(LocalTime.of(23,59), natAntal);
+    }
 
     @Override
     public double doegnDosis() {
-        return 0;
+        double sum = 0;
+        for (Dosis dosis : doser) {
+            sum += dosis.getAntal();
+        }
+        return sum;
     }
 
     @Override
     public double samletDosis() {
-        return 0;
+        return doegnDosis() * antalDage();
     }
 
     @Override
@@ -27,6 +39,6 @@ public class DagligFast extends Ordination{
 
     @Override
     public String getType() {
-        return
+        return "DagligFast";
     }
 }
