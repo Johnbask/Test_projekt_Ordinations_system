412404b67afdcb34638cf2ab8e2d1a8aa53e00b0
AkselTimmermann <aksel.timmermann@gmail.com>
2026-03-05 09:59:54 +0100

    DagligFast metoder oprettet

--- src/ordination/DagligFast.java
+++ src/ordination/DagligFast.java
@@ -1,5 +1,32 @@
 package ordination;
 
-public class DagligFast {
+
+import gui.TypeOrdination;
+
+import java.time.LocalDate;
+
+public class DagligFast extends Ordination{
     // TODO
+    private Dosis[] doser;
+
+
+    @Override
+    public double doegnDosis() {
+        return 0;
+    }
+
+    @Override
+    public double samletDosis() {
+        return 0;
+    }
+
+    @Override
+    public int antalDage() {
+        return super.antalDage();
+    }
+
+    @Override
+    public String getType() {
+        return TypeOrdination;
+    }
 }
