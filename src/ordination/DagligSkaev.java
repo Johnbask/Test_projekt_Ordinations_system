2aa6458ad11f37eafa810d37a3957a3b602994ab
CappiDell <caper.christensen@gmail.com>
2026-03-05 09:59:43 +0100

    Lavet lidt

--- src/ordination/DagligSkaev.java
+++ src/ordination/DagligSkaev.java
@@ -1,12 +1,32 @@
 package ordination;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
 
-public class DagligSkaev {
-    // TODO
+public class DagligSkaev extends Ordination {
+    private ArrayList<Dosis> dosisArrayList = new ArrayList<>();
 
+    public DagligSkaev() {
+    }
 
     public void opretDosis(LocalTime tid, double antal) {
-        // TODO
+        Dosis dosis = new Dosis(tid,antal);
+        dosisArrayList.add(dosis);
+    }
+
+    @Override
+    public double samletDosis() {
+        return 0;
+    }
+
+    @Override
+    public double doegnDosis() {
+        double doegndosis = 0;
+        for ()
+    }
+
+    @Override
+    public String getType() {
+        return "";
     }
 }
